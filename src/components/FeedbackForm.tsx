f7f9d5e4cca69f68923f6d3b7cc12202ecf08b49
Kwaku Asetena-Krah <kwakukrah.da@gmail.com>
2024-12-25 13:23:38 +0000

    commit

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
+import { MAX_CHARACTER } from "../lib/constants";
+
 export default function FeedbackForm() {
+  const [text, setText] = useState("");
+  const characterCount = MAX_CHARACTER - text.length;
+
   return (
     <form className="form">
-      <textarea id="feedback-textarea" placeholder="true" spellCheck={false} />
+      <textarea
+        id="feedback-textarea"
+        placeholder="true"
+        spellCheck={false}
+        value={text}
+        onChange={(e) => {
+          const newText = e.target.value;
+          if (newText.length > MAX_CHARACTER) {
+            return;
+          }
+          setText(newText);
+        }}
+      />
       <label htmlFor="feedback-textarea">
         Enter your feedback here, remember to #hashtag the company
       </label>
       <div>
-        <p className="u-italic">150</p>
+        <p className="u-italic">{characterCount}</p>
         <button>
           <span>Submit</span>
         </button>
